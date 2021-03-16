d7981a6244413dc844c06761b88d24b0c9af7d86
agohdasol <agohdasol@gmail.com>
2021-03-16 23:32:42 +0900

    C ~119p

--- Ch04/main.c
+++ Ch04/main.c
@@ -9,8 +9,20 @@ void ch04_01(void)
   printf("%.2f", (a + b) / 2.0);
 }
 
+void ch04_02(void)
+{
+  int input_time;
+  printf("정수 초를 입력하세요. : ");
+  scanf("%d", &input_time);
+
+  int hours = input_time / 3600;
+  int minutes = (input_time % 3600) / 60;
+  int seconds = (input_time % 3600) % 60;
+  printf("%d 초는 %d 시간 %d 분 %d 초입니다.", input_time, hours, minutes, seconds);
+}
+
 int main(void)
 {
-  ch04_01();
+  ch04_02();
   return 0;
 }
